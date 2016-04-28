a989396d96990a12463a550b496cd02aaa9eda0f
erikVildanov <erik_bot@icloud.com>
2016-04-28 12:17:26 +0600

    awe

---
+++ LostFilm.tv/LostFilm.tv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LostFilm.tv
+//
+//  Created by Эрик Вильданов on 28.04.16.
+//  Copyright © 2016 Эрик Вильданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
